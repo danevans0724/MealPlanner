df34a0dc2d08d9fad28c9791ace36c70990cfae9
pmidce0 <pmidce0@DTW122210N02.nasa.cpwr.corp>
2017-04-26 19:42:02 -0400

    Updated project to add handlers, menus, and commands

---
+++ org.evansnet.ingredient/src/org/evansnet/ingredient/handlers/IngredientSaveHandler.java
@@ -0,0 +1,12 @@
+package org.evansnet.ingredient.handlers;
+
+
+/**
+ * Handles requests to save the ingredient to the database.
+ * 
+ * @author pmidce0
+ *
+ */
+public class IngredientSaveHandler {
+
+}
---
+++ org.evansnet.ingredient/src/org/evansnet/ingredient/ui/IngredientExplorerView.java
@@ -0,0 +1,27 @@
+package org.evansnet.ingredient.ui;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+
+/**
+ * Provides a tree view for navigating Ingredient types and ingredients in 
+ * the system. 
+ * @author pmidce0
+ *
+ */
+public class IngredientExplorerView extends ViewPart {
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
